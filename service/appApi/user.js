78677b5192fa21e5db0b1e9cf0caf9fac13430ce
js-zahgn <156231639@qq.com>
2018-09-20 17:41:30 +0800

    Update user.js
    将用户头像上传至Nginx服务器

--- service/appApi/user.js
+++ service/appApi/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const svgCaptcha = require('svg-captcha')
 let router = new Router()
 const bcrypt = require('bcrypt-nodejs')
+const fs = require('fs')
 const SALT_WORK_FACTOR = 10
 
 const createToken = require('../token/createToken.js');
@@ -21,24 +22,20 @@ router.get('/getVerificationCode', async(ctx) => {
       // background: '#cc9966'
     }
     const cap = svgCaptcha.create(codeConfig)
-
+    const date = new Date()
     ctx.cookies.set('CheckCode', cap.text.toLowerCase(), {
       // domain: '127.0.0.1',
       // path: '/register',
       maxAge: 300000,
-      expires: new Date('2018-12-31'),
+      expires: new Date(`${date.getFullYear()}-12-31`),
       httpOnly: false,
       overwrite: false
     })
-    ctx.status = 200
-    ctx.body = {
-      data: cap.data
-    }
+    ctx.status = 500
+    ctx.body = { data: cap.data }
   } catch (err) {
-    ctx.status = 500;
-    ctx.body = {
-      data: `获取验证码失败-->${err}`
-    }
+    ctx.status = 500
+    ctx.body = { data: `获取验证码失败-->${err}` }
   }
 })
 
@@ -50,7 +47,7 @@ router.post('/register', async(ctx) => {
     const User = mongoose.model('User')
     // 接收前端传输的数据，并封装成新的user对象
     const {body} = ctx.request;
-    if(CheckCode != body.code) {
+    if (CheckCode != body.code) {
       ctx.status = 500
       ctx.body = {data: '验证码错误，请输入正确验证码'}
       return
@@ -119,7 +116,7 @@ router.post('/login', async(ctx) => {
         if(isMatch) {
           const token = createToken(name)
           const set = {$set: {lastLoginAt: new Date().getTime(), token: token}}
-          await User.updateOne({userName: name}, set).exec().then(() => {
+          await User.updateOne({ userName: name }, set).exec().then(() => {
             ctx.status = 200
             ctx.body = {data: isMatch, token: token}
           })
@@ -132,7 +129,7 @@ router.post('/login', async(ctx) => {
         ctx.status = 500
         ctx.body = {data: err}
       })
-    }else{
+    } else {
       ctx.status = 500
       ctx.body = {data: '用户名不存在'}
     }
@@ -147,7 +144,7 @@ router.get('/getUserInfo', async(ctx) => {
   try {
     const User = mongoose.model('User')
     const name = ctx.request.header.username;
-    await User.findOne({userName: name}).exec().then(res => {
+    await User.findOne({ userName: name }).exec().then(res => {
       ctx.status = 200
       ctx.body = {
         data: {
@@ -156,35 +153,80 @@ router.get('/getUserInfo', async(ctx) => {
         }
       }
     })
-  } catch(err) {
+  } catch (err) {
     ctx.status = 401
-    ctx.body = {
-      data: '获取用户信息失败-->' + err
-    }
+    ctx.body = { data: '获取用户信息失败-->' + err }
   }
 })
 
 // 上传用户头像
 router.post('/uploadUserImg', async(ctx) => {
   try {
+    const { img, name } = ctx.request.body;
+    const { username } = ctx.request.headers
+    const dd = new Date()
+    const dateNow = dd.getTime().toString(16)
+    const diskUrl = `F:/n_files/images/${username}`
+    const targetUrl = `${diskUrl}/${dateNow}_${name}`
+    // 如果文件夹不存在就新建此文件夹
+    if (!fs.existsSync(diskUrl)) {
+      fs.mkdirSync(diskUrl, err => {
+        if (err) {
+          ctx.status = 500
+          ctx.body = { data: '上传头像失败-->' + err }
+        }
+      })
+    }
+    const nginxHost = 'http://127.0.0.1:2020/images'
     const User = mongoose.model('User')
-    const {img} = ctx.request.body
-    const name = ctx.request.headers.username
-    const set = {$set: {userImg: img}}
-    await User.updateOne({userName: name}, set).exec().then(() => {
+    const set = { $set: { userImg: `${nginxHost}/${username}/${dateNow}_${name}` } }
+    await User.updateOne({ userName: username }, set).exec().then(async() => {
+      const dataBuffer = Buffer.from(img.replace(/^data:image\/\w+;base64,/, ''), 'base64')
+      await new Promise(resolve => {
+        fs.writeFile(targetUrl, dataBuffer, (err) => {
+          if (err) {
+            ctx.status = 500
+            ctx.body = { data: '上传头像失败-->' + err }
+          }
+        })
+        resolve()
+      })
       ctx.status = 200
       ctx.body = {
         data: {
-          userName: name,
-          userImg: img
+          userName: username,
+          userImg: set.$set.userImg
         }
       }
     })
   } catch (err) {
     ctx.status = 500
-    ctx.body = {
-      data: '上传头像失败-->' + err
-    }
+    ctx.body = { data: '上传头像失败-->' + err }
+  }
+})
+
+// 用过的头像
+router.get('/getUsedUserImg/:name', async(ctx) => {
+  try {
+    let imgArr = []
+    const diskUrl = `F:/n_files/images/${ctx.params.name}`
+    const nginxHost = `http://127.0.0.1:2020/images/${ctx.params.name}`
+    await new Promise(resolve => {
+      fs.readdir(diskUrl, (err, file) => {
+        if (err) {
+          ctx.status = 500
+          ctx.body = { data: '读取文件失败->' + err }
+        }
+        imgArr = file
+        resolve()
+      })
+    })
+
+    ctx.status = 200
+    ctx.body = imgArr.map(img => `<img src="${nginxHost}/${img}" width="100px"/>`).join(',')
+  } catch (err) {
+    ctx.status = 500
+    ctx.body = {data: '获取头像失败-->' + err}
   }
 })
 
